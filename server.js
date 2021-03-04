dad116d3044673a7176d3416fa42fd6b5363089e
banuakman <banu@asterya.com>
2021-03-04 14:44:43 -0500

    create initial files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// concet to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// login middleware
+app.use(logger("dev"));
+
+// post request middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static folder
+app.use(express.static("public"));
+
+// routes
+
+app.listen(PORT, () => {
+  console.log(`You are up & running on port ${PORT}!`);
+});
