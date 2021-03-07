ad994f075496bd1437c8c2482ec6c9a07d8d4bc9
banuakman <banu@asterya.com>
2021-03-07 14:58:32 -0500

    correct routes connection

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // routes
-app.use(require("./routes/html-routes"));
-app.use(require("./routes/api-routes.js"));
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 app.listen(PORT, () => {
 	console.log(`You are up & running on port ${PORT}!`);
 });
